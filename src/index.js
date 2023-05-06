968bf66425a5594d93a28ffe4f995bc18227ee15
userzahar <110730909+userzahar@users.noreply.github.com>
2023-05-06 21:29:37 +0300

    fix bug BrowserRouter

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/violet-cards">
+    <BrowserRouter basename="/violet-cardss">
       <App />
     </BrowserRouter>
   </React.StrictMode>
