c59b834a6c0960ac51c48114f9573b770445ed6c
userzahar <110730909+userzahar@users.noreply.github.com>
2023-05-07 13:56:56 +0300

    refractoring app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,33 @@
-import { Route, Routes } from "react-router-dom"
-import { lazy } from "react";
-import { Header } from "./Header/Header";
+import { Suspense, lazy } from "react";
+import {  Route, Routes } from "react-router-dom"
+import { HeaderLink, HeaderStyled } from "./App.styled";
 
 const Homepage = lazy(() => import('pages/Homepage/Homepage'));
 const Tweets = lazy(() => import('pages/Tweets/Tweets'));
 
 export const App = () => {
-  return (
+  return (<div>
+         <HeaderStyled>
+      <nav>
+        <HeaderLink to="/" end>Home</HeaderLink>
+        <HeaderLink to="/tweets">Tweets</HeaderLink>
+      </nav>
+    </HeaderStyled>
+    <main>
+      <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+              <Route path="/">
+                    <Route index element={<Homepage />} />
+                    <Route path="*" element={<Homepage />} />
+                    <Route path="/tweets" element={<Tweets />} />
+              </Route>
+          </Routes>
+      </Suspense>
+    </main> 
+  </div>
 
-      <Routes>
-          <Route path="/" element={<Header/>}>
-                <Route index element={<Homepage />} />
-                <Route path="*" element={<Homepage />} />
-                <Route path="/tweets" element={<Tweets />} />
-          </Route>
-      </Routes>   
 
+        
   );
 };
 
--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 export const ContainerStyled = styled.div`
   display: flex;
   flex-direction: column;
@@ -13,4 +14,27 @@ export const GalleryContainerStyled = styled.ul`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-evenly;
+`;
+export const HeaderStyled = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #fff;
+  padding: 1rem;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  & nav {
+    display: flex;
+    gap: 1rem;
+  }
+`;
+export const HeaderLink = styled(NavLink)`
+  color: #555;
+  text-decoration: none;
+  font-weight: bold;
+  padding: 0.5rem;
+  border-radius: 0.25rem;
+  transition: background-color 0.1s ease-in-out;
+  & :hover {
+    background-color: #f4f4f4;
+  }
 `;
